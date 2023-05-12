fc643683add8b6a85eedab84e2e08cabcccf89fe
nijaskhan <nijaskhan93@gmail.com>
2023-05-12 15:28:30 +0530

    finished

--- server/controllers/adminControllers.js
+++ server/controllers/adminControllers.js
@@ -2,68 +2,90 @@ const jwt = require('jsonwebtoken');
 const Admin = require("../models/adminModel");
 const User = require('../models/userModel');
 
-module.exports ={
-    login: async(req, res)=>{
-        try{
-            const admin = await Admin.findOne({email: req.body.email}).exec();
-            if(admin){
-                if(admin.password===req.body.password){
-                    const token = jwt.sign({adminId: admin._id}, process.env.JWT_SECRET, {expiresIn: "1d"});
+module.exports = {
+    login: async (req, res) => {
+        try {
+            const admin = await Admin.findOne({ email: req.body.email }).exec();
+            if (admin) {
+                if (admin.password === req.body.password) {
+                    const token = jwt.sign({ adminId: admin._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
                     res.send({
                         success: true,
                         message: "admin logged in succesfully",
                         data: token,
                         admin: admin
                     });
-                }else{
+                } else {
                     throw new Error("Invalid password !!");
                 }
-            }else{
+            } else {
                 throw new Error("Invalid Credentials !!");
             }
-        }catch(err){
+        } catch (err) {
             res.send({
                 success: false,
                 message: err.message
             });
         }
     },
-    getAllUsers: async(req, res)=>{
-        try{
+    getAllUsers: async (req, res) => {
+        try {
             const users = await User.find().exec();
             res.send({
                 success: true,
                 message: "fetched all users",
                 users
             })
-        }catch(err){
+        } catch (err) {
             res.send({
                 success: false,
                 message: err.message
             })
         }
     },
-    deleteUser: (req, res)=>{
-        try{
-            User.deleteOne({_id: req.params.id})
-            .then(()=>{
-                res.send({
-                    success: true,
-                    message: "user deleted successfully"
-                });
-            })
-            .catch((err)=>{
-                throw new Error(err.message);
-            })
-        }catch(err){
+    deleteUser: (req, res) => {
+        try {
+            User.deleteOne({ _id: req.params.id })
+                .then(() => {
+                    res.send({
+                        success: true,
+                        message: "user deleted successfully"
+                    });
+                })
+                .catch((err) => {
+                    throw new Error(err.message);
+                })
+        } catch (err) {
             res.send({
                 success: false,
                 message: err.message
             });
         }
     },
-    updateUser: (req, res)=>{
-        console.log("halle luha");
-        console.log(req.body);
+    updateUser: (req, res) => {
+        try {
+            console.log(req.body);
+            User.updateOne({_id: req.body.data.editId},
+                {
+                    name: req.body.data.editName,
+                    email: req.body.data.editEmail,
+                    mobile: req.body.data.editMobile
+                })
+            .then((response)=>{
+                console.log(response);
+                res.send({
+                    success: true,
+                    message: "user updated"
+                });
+            })
+            .catch((err)=>{
+                throw new Error(err.message);
+            })
+        } catch (err) {
+            res.send({
+                success: false,
+                message: err.message
+            })
+        }
     }
 }
