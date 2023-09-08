75525648f85387eff00711c96e8b860f2b4d0369
nijaskhan <nijaskhan93@gmail.com>
2023-09-08 14:30:23 +0530

    added some packages for json convertion

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig');
 const port = process.env.PORT || 5000;
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 const adminRoute = require('./routes/adminRouter');
 const usersRoute = require('./routes/userRoutes');
